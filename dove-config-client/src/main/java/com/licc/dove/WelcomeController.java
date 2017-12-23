7baa8bee12b3de3f373fbab60e8f663cd49e8fda
licc168 <liccwork@126.com>
2017-12-23 16:37:16 +0800

    spring cloud config jdbc 方式

--- dove-config-client/src/main/java/com/licc/dove/WelcomeController.java
+++ dove-config-client/src/main/java/com/licc/dove/WelcomeController.java
@@ -1,13 +1,14 @@
-package com.jumore;
+package com.licc.dove;
 
 import java.util.Map;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RefreshScope
 public class WelcomeController {
 
 
